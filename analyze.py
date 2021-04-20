8ad1b71f5ea31d31fb981759a8baabd09665969b
LengyHELL <ubi19998@gmail.com>
2021-04-20 21:58:29 +0200

    Implementing ball detection

--- analyze.py
+++ analyze.py
@@ -21,6 +21,11 @@ def is_inside(inside, outside, limit_val=-1):
                 return True
     return False
 
+def get_edges(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thr = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[0]
+    return cv2.Canny(gray, 1.0 * thr, 0.5 * thr)
+
 start_time = time.time()
 
 # checking arguments
@@ -58,24 +63,93 @@ if (cap.isOpened() == False):
     print("Error opening input video!")
     sys.exit(4)
 
+play = True
+ret = True
+
 while (cap.isOpened()):
-    ret, frame = cap.read()
     if ret == True:
-        # Start processing frame
+        if play:
+            ret, frame = cap.read()
+            # Start processing frame
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            #frame = cv2.fastNlMeansDenoisingColored(frame, None, 15, 10, 7, 21)
 
-        lower_green = np.array([40, 190, 50])
-        upper_green = np.array([65, 255, 225])
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(hsv, lower_green, upper_green)
-        res = cv2.bitwise_and(frame, frame, mask = mask)
+            lower_green = np.array([40, 190, 50])
+            upper_green = np.array([65, 255, 225])
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+            res = cv2.bitwise_and(frame, frame, mask = mask)
 
-        cv2.imshow("Frame", res)
+            edges = get_edges(res)
+            kernel = np.array([ [0, 0, 1, 0, 0],
+                                [0, 0, 1, 0, 0],
+                                [0, 0, 1, 0, 0],
+                                [0, 0, 1, 0, 0],
+                                [0, 0, 1, 0, 0]], dtype=np.uint8)
 
-        # End processing frame
+            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel, iterations = 1)
+
+            kernel = np.array([ [0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0],
+                                [1, 1, 1, 1, 1],
+                                [0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0]], dtype=np.uint8)
+            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel, iterations = 1)
+
+            cnt, hier = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+
+            if len(cnt) > 0:
+                table = sorted(cnt, key=lambda x : cv2.contourArea(x), reverse=True)[0]
+
+                hull = cv2.convexHull(table)
+
+                # (table area / 760) <= ball area <= (table area / 775)
+                table_area = cv2.contourArea(hull)
+                #cnt = cnt[1:]
+                #balls = []
+                #for c in cnt:
+                #    area = cv2.contourArea(c)
+                #    lower = table_area / 1200
+                #    upper = table_area / 700
+                #    if lower <= area <= upper:
+                #        balls.append(c)
+
+                params = cv2.SimpleBlobDetector_Params()
+
+                params.filterByArea = True
+                params.minArea = table_area / 2000 #1200
+                params.maxArea = table_area / 700 #700
+
+                params.filterByCircularity = True
+                params.minCircularity = 0.4 #0.1
+                params.maxCircularity = 1
+
+                params.filterByConvexity = True
+                params.minConvexity = 0.87
+
+                detector = cv2.SimpleBlobDetector_create(params)
+                keypoints = detector.detect(edges)
+
+                #mask = np.zeros(edges.shape, np.uint8)
+                #mask = cv2.drawContours(mask, [hull], -1, 255, -1)
+                #board = cv2.bitwise_and(frame, frame, mask = mask)
+
+                cv2.drawContours(frame, [hull], -1, (125, 0, 125), 2)
+                #cv2.drawContours(frame, keypoints, -1, (255, 0, 0), 1)
+                frame = cv2.drawKeypoints(frame, keypoints, np.array([]), (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+            cv2.imshow("Frame", frame)
+
+            # End processing frame
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
+        if (cv2.waitKey(33) & 0xFF == ord("a")) and (not key_lock):
+            play = not play
+            key_lock = True
+        if not (cv2.waitKey(33) & 0xFF == ord("a")):
+            key_lock = False
     else:
         break
 
