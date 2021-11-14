aa6066b6706c25c36c38f85d27a903e87a2a6f8f
LengyHELL <ubi19998@gmail.com>
2021-11-14 17:34:53 +0100

    Completing dataset creator template matching

--- create_dataset.py
+++ create_dataset.py
@@ -1,9 +1,11 @@
 import os
+
 import time
+import numpy as np
 import matplotlib.pyplot as plt
 
 import cv2
-from analyze import load_image, find_table, cut_and_warp, search_template
+from analyze import load_image, find_table, cut_and_warp, search_template, calculate_diff
 
 # http://www.flyordie.hu/snooker/
 
@@ -22,23 +24,45 @@ for f in files:
     img_orig = img.copy()
 
     tmp = load_image("misc/templates/black_ball_hd.png")
-    black   = search_template(img, tmp, threshold=0.855, min_diff=15) #ok
-    #tmp = load_image("misc/templates/blue_ball_hd.png")
-    #blue    = search_template(img, tmp, threshold=0.92, min_diff=15) #ok
-    #tmp = load_image("misc/templates/brown_ball_hd.png")
-    #brown   = search_template(img, tmp, threshold=0.92, min_diff=15) #not ok
-    #tmp = load_image("misc/templates/green_ball_hd.png")
-    #green   = search_template(img, tmp, threshold=0.92, min_diff=15)
-    #tmp = load_image("misc/templates/pink_ball_hd.png")
-    #pink    = search_template(img, tmp, threshold=0.92, min_diff=15)
-    #tmp = load_image("misc/templates/red_ball_hd.png")
-    #red     = search_template(img, tmp, threshold=0.92, min_diff=15) #ok
-    #tmp = load_image("misc/templates/white_ball_hd.png")
-    #white   = search_template(img, tmp, threshold=0.92, min_diff=15)
-    #tmp = load_image("misc/templates/yellow_ball_hd.png")
-    #yellow  = search_template(img, tmp, threshold=0.92, min_diff=15)
+    black = search_template(img, tmp, threshold=0.855, min_diff=15) #ok
 
-    points = black
+    tmp = load_image("misc/templates/blue_ball_hd.png")
+    blue = search_template(img, tmp, threshold=0.92, min_diff=15) #ok
+
+    tmp = load_image("misc/templates/brown_ball_hd.png")
+    brown = search_template(img, tmp, threshold=0.905, min_diff=15) #ok
+
+    tmp = load_image("misc/templates/green_ball_hd.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2HSV)
+    green = search_template(img, tmp, threshold=0.985, min_diff=15) #ok
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+
+    tmp = load_image("misc/templates/pink_ball_hd.png")
+    pink = search_template(img, tmp, threshold=0.97, min_diff=15) #ok
+
+    tmp = load_image("misc/templates/red_ball_hd.png")
+    red = search_template(img, tmp, threshold=0.92, min_diff=15) #ok
+
+    tmp = load_image("misc/templates/white_ball_hd.png")
+    white = search_template(img, tmp, threshold=0.97, min_diff=15) #ok
+
+    tmp = load_image("misc/templates/yellow_ball_hd.png")
+    yellow = search_template(img, tmp, threshold=0.95, min_diff=15) #ok
+
+    # correction for pesky brown ball
+    temp = brown
+    brown = []
+    for t in temp:
+        add = True
+        for r in red:
+            diff = calculate_diff(t, r)
+            if diff < 15:
+                add = False
+        if add:
+            brown.append(t)
+    brown = np.array(brown)
+    points = brown
     for p in points:
         p = (p[0] - 10, p[1] - 10)
         cv2.rectangle(img, p, (p[0] + 20, p[1] + 20), (0, 0, 255), 2)
